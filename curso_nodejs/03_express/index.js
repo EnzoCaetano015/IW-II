5f96a60ef2f718cb9fcba7acf6930168f48f6348
EnzoCaetano015 <peracioenzo@gmail.com>
2024-05-20 13:53:30 -0300

    node js

--- curso_nodejs/03_express/index.js
+++ curso_nodejs/03_express/index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 
 app.get('/',function(req,res){
-    res.send('seja bem vindo baitola!')
+    res.send('seja bem vindo !')
 })
 
 app.get('/blog',function(req,res){
